c020e58e370c54dfcb1383d1d2137d8ea21a1cf2
Shuji-Watanabe <shuji-watanabe@neptune.kanazawa-it.ac.jp>
2024-09-13 17:56:25 +0900

    20240913-17

---
+++ 2024年第2回高大連携定例研究会/libraries/cal.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+class BasicStatistics:
+
+    def average(self):
+        return self.a + self.b
+
+    def set(self, a, b):
+        self.a = a
+        self.b = b
