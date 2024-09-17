46187817c039731158cd1c00b3d417ad5d2d4c48
Shuji-Watanabe <shuji-watanabe@neptune.kanazawa-it.ac.jp>
2024-09-17 11:26:21 +0900

    ver 02

--- 2024年第2回高大連携定例研究会/pages/01_テキストの表示.py
+++ 2024年第2回高大連携定例研究会/pages/01_テキストの表示.py
@@ -9,6 +9,8 @@ _streamlit_ で使えるテキストの表示方法を紹介します．
 _streamlit_ でテキストの修飾を行いたい場合，基本的には 
 [_Markdown_ 記法](https://help.qast.jp/ja/articles/9026060-markdown記法で装飾する) 
 に従った書き方で修飾が可能です．
+
+
 """
 
 #st: st.header を利用した見出し表示
@@ -27,23 +29,20 @@ if st.checkbox("ソースコード表示",key="titile_code_disp") :
     """
     st.code(title_code, language="python")
     """
-    この機能の説明はこちらから（[ API reference ](https://docs.streamlit.io/develop/api-reference/text/st.title)）
+    __参考資料__  
+    - `st.title`に関する詳細はこちらをご覧ください．  
+        [ API reference:title ]( https://docs.streamlit.io/develop/api-reference/text/st.title )
     """
+"""___"""
 
 #st: st.header を利用した見出し表示
 st.header("Magic commandを利用した見出し",divider='rainbow')
 
 #st: Magic commandを利用したテキスト表示
 """
-下の見出しは
-[ _Magic command_ ](https://docs.streamlit.io/develop/api-reference/write-magic/magic)
-を使用して表示しています．
+下の見出しは Magic command を使用して表示しています．
 
 #  文章表示のあれこれ
-
-これは streamlit 特有の記法です．
-表示したい文章をトリプルクォーテーション（\"\"\"）で囲むことで，Webアプリ上に文字列が
-表示されます．
 """
 
 #st: Magic command　を利用したテキスト表示のソースコードを表示させる
@@ -59,3 +58,13 @@ if st.checkbox("ソースコード表示",key="titile_magic_code_disp") :
     ハッシュを入力し、半角スペースを空けて文字を入力すると見出しとして表示されます．
     また，ハッシュの後に必ずハッシュの数が増えると文字が小さくなります．
     """
+
+"""
+これは streamlit 特有の記法です．Markdown記法を用いて表示したい文章を作成し，それをトリプルクォーテーション（\"\"\"）で囲むことで，Webアプリ上に文字列が
+表示されます．
+
+__参考資料__  
+- Magic commandの詳細はこちらをご覧下さい．  
+    [ API reference:Magic command ](https://docs.streamlit.io/develop/api-reference/write-magic/magic)
+"""
+"""___"""
