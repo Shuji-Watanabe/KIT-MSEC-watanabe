0d77eff0129d27d3a17c2970cdb423b6f1053559
Shuji-Watanabe <shuji-watanabe@neptune.kanazawa-it.ac.jp>
2024-09-18 09:50:17 +0900

    20240918-ver014

--- 2024年第2回高大連携定例研究会/pages/05_Map機能.py
+++ 2024年第2回高大連携定例研究会/pages/05_Map機能.py
@@ -12,7 +12,8 @@ streamlitには`st.map`という地図機能がありますが，地図情報が
 個人でライセンスを取得することが推奨されている機能となっています．
 利便性を考慮し，この機能についての説明は割愛しました．
 
-ここでは，オープソースのmap情報を利用した，地図機能を紹介します．これを利用するためには
+ここでは，オープソースのmap情報を利用した，地図の表示方法を紹介します．
+なお，この機能を利用するためには
 『folium』および『streamlit_folium』というライブラリのインストールが必要となります．
 
 __参考資料__
@@ -23,11 +24,11 @@ __参考資料__
 - streamlit_foliumとfoliumに関する詳細はこちらをご覧ください．  
     [Zenn：Streamlitで世界の太陽光発電所情報を可視化](https://zenn.dev/ta_murata_18/articles/8d63b7aae9a4b9)
 - foliumと同様の地図機能を用いた情報Ⅱにおける実践例がありましたので，情報共有致します．
-    文部科学省，高等学校「情報」実践事例集，[情報II(5)「地図コンテンツを活用して平和問題への理解を深めよう」]
+    文部科学省，高等学校「情報」実践事例集，[情報II(5)「地図コンテンツを活用して平和問題への理解を深めよう」](https://www.mext.go.jp/content/20220324-mxt_kouhou02-000021508_2.pdf)
 """
 
 #st: st.header を利用した見出し表示
-st.header("1. folium を利用した画像の表示方法",divider='rainbow')
+st.header("1. folium を利用した地図の表示方法",divider='rainbow')
 
 # 初期表示に関する設定
 map = folium.Map(location=[36.530, 136.6287],zoom_start = 15) 
@@ -45,8 +46,6 @@ folium.Marker(
 # mapに記された情報をstreanlit上に表示
 st_folium(map)
 
-
-""" CHeck"""
 if st.checkbox("ソースコード表示",key="show_code_disp1") :
     """"""
     st.code("""
@@ -72,7 +71,7 @@ if st.checkbox("ソースコード表示",key="show_code_disp1") :
 
 ### 散歩データの表示
 #st: st.header を利用した見出し表示
-st.header("2. folium を利用した画像の表示方法（応用編）",divider='rainbow')
+st.header("2. folium の応用",divider='rainbow')
 
 """
 ここではスマートフォンで取得した散歩中の位置情報を，地図上に示す方法を紹介します．
@@ -80,8 +79,8 @@ st.header("2. folium を利用した画像の表示方法（応用編）",divide
 __参考資料__
 - スマートフォンによる位置情報の取得はphyphoxを利用しました．
     [phyphox](https://phyphox.org)
-
-
+\n 
+\n
 """
 
 #地図上に表示する情報の取得（GPSデータの読み込み）
@@ -138,6 +137,7 @@ line1 = folium.PolyLine(linedata_list , weight=5, color="#0000ff").add_to(map)
 
 """
 #### 散歩中の位置情報と移動経路
+
 """
 
 #　mapに格納された情報をstreamlit上に表示
