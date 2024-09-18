b867660d1f56a027a0caa201cfafe8857c9324bf
Shuji-Watanabe <shuji-watanabe@neptune.kanazawa-it.ac.jp>
2024-09-18 10:16:06 +0900

    20240918-ver016

--- 2024年第2回高大連携定例研究会/pages/05_Map機能.py
+++ 2024年第2回高大連携定例研究会/pages/05_Map機能.py
@@ -142,3 +142,61 @@ line1 = folium.PolyLine(linedata_list , weight=5, color="#0000ff").add_to(map)
 
 #　mapに格納された情報をstreamlit上に表示
 folium_static(map)
+
+if st.checkbox("ソースコード表示",key="show_code_disp1") :
+    """"""
+    st.code("""
+            #地図上に表示する情報の取得（GPSデータの読み込み）
+            try :
+                df = pd.read_csv('sample_datas/GPS_Data.csv')
+            except:
+                df = pd.read_csv('2024年第2回高大連携定例研究会/sample_datas/GPS_Data.csv')
+            # dfのキー（列名）を取得
+            df_key_list = df.keys()
+
+
+            # 初期表示に関する設定
+            map = folium.Map(location=[36.53183045, 136.6286506],zoom_start = 18) 
+
+            # dfから１行ずつ位置情報を取得し，その情報を地図上のマーカーに記載
+            for i, row in df.iterrows():
+                # 地図へのマーカーの追加
+                if i == 0 :
+                    # ポップアップに関する変数の作成
+                    pop=f"Start <br>　時間…{row[df_key_list[0]]:,} s <br>　速さ…{row[df_key_list[3]]:,}m/s"
+                    folium.Marker(
+                        # 緯度と経度を指定
+                        location=[row[df_key_list[1]], row[df_key_list[2]]],
+                        # ポップアップの指定
+                        popup=folium.Popup(pop, max_width=200),
+                        # アイコンの指定(アイコン、色)
+                        icon=folium.Icon(icon="flag",icon_color="white", color="red")
+                    ).add_to(map)
+
+                    
+                elif i==len(df)-1:
+                    pop=f"End <br>　時間…{row[df_key_list[0]]:,} s <br>　速さ…{row[df_key_list[3]]:,}m/s"
+                    folium.Marker(
+                        # 緯度と経度を指定
+                        location=[row[df_key_list[1]], row[df_key_list[2]]],
+                        # ポップアップの指定
+                        popup=folium.Popup(pop, max_width=200),
+                        # アイコンの指定(アイコン、色)
+                        icon=folium.Icon(icon="flag",icon_color="white", color="blue")
+                    ).add_to(map)
+                else :
+                    pop=f"<br>　時間…{row[df_key_list[0]]:,} s <br>　速さ…{row[df_key_list[3]]:,}m/s"
+                    folium.Marker(
+                        # 緯度と経度を指定
+                        location=[row[df_key_list[1]], row[df_key_list[2]]],
+                        # ポップアップの指定
+                        popup=folium.Popup(pop, max_width=200),
+                        # アイコンの指定(アイコン、色)
+                        icon=folium.Icon(icon="flag",icon_color="white", color="gray")
+                    ).add_to(map)
+
+            linedata_list = df[[df_key_list[1],df_key_list[2]]].values.tolist()
+            line1 = folium.PolyLine(linedata_list , weight=5, color="#0000ff").add_to(map)
+            """
+            , language="python")
+
