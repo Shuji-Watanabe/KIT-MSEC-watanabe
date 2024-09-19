b071230b1119a28f1751a63d078190ccdee3924c
Shuji-Watanabe <shuji-watanabe@neptune.kanazawa-it.ac.jp>
2024-09-19 11:19:52 +0900

    20240919-ver012

--- 2024年第2回高大連携定例研究会/pages/10_📚streamlitの始め方.py
+++ 2024年第2回高大連携定例研究会/pages/10_📚streamlitの始め方.py
@@ -11,36 +11,8 @@ import streamlit as st
 
 今回の説明はvscodeを使用した場合の説明です．
 
-__参考資料__
-- vscodeのインストール方法(Windows)  
-    [みやしもブログ：VSCodeのインストール方法・日本語化の手順を解説する](https://miyashimo-studio.jp/blog/detail/vscode-install/)
-- vscodeのインストール＋pythonのインストール方法(Windows)  
-    [Visual Studio CodeをインストールしてPython開発環境を準備する(Windows編)](https://sukkiri.jp/technologies/processors/python/visual-studioをインストールしてpython開発環境を準備するwindows編.html)
-- pythonのバージョンについて  
-    pythonはそのバージョンごとにインストールできるライブラリが異なるのでご注意ください．
-    本発表のアプリ開発には, _python 3.12.0_ を使用しています．
-- pipのバージョンについて  
-    pythonのライブラリをインストールする際，パッケージ管理システムpipを利用します．
-    本発表のアプリ開発には, _pip 24.2_ を使用しています．pipの利用については，
-    次のリンク先の『3.3. packageのインストール』をご覧ください．  
-    [Qiita:[python] pip/VSCode開発環境の構築 (windows11)](https://qiita.com/flcn-x/items/ac6e222004a827f582ea)
-- streamlitのバージョンについて  
-    streamlitは現在も活発な開発が行われており，バージョンごとに使えるオプションが異なります．
-    streamlitのAPI referenceで紹介されている機能は，概ね最新版のものです．
-    本発表のアプリ開発には, _streamlit 1.38.0_ を使用しています．
-- streamlitの基本的な使い方について  
-    streamlitのAPI referenceで詳しい説明がなされています．導入については，
-    _Get started_に記載してあります．
-    [streamlit API reference](https://docs.streamlit.io)
-- streamlit Community Cloud を利用した一般公開について  
-    streamlit Community Cloudを利用することで作成したアプリを一般公開することが可能です．
-    そのための手順は次の通りです．
-    1. GitHubのアカウント作成
-    1. GitHubにリポジトリを作成
-    1. GitHubのリポジトリにWebアプリのソースファイル等をアップロード
-    1. streamlit Community Cloudのアカウント作成
-    1. streamlit Community CloudでWebアプリをデプロイ
 """
+
 st.info("""詳細は今後アップデートします""")
 
 """  """
@@ -97,5 +69,34 @@ vscodeのターミナルで，streamlitのソースファイルがある場所�
 と表示されるはずです． _XXXXXXX_ や _YYYYYYY_ を利用することで，
 同一ネットワーク上にあるPCから，このアプリが利用できます．
          """)
-
-
+"""
+__参考資料__
+- vscodeのインストール方法(Windows)  
+    [みやしもブログ：VSCodeのインストール方法・日本語化の手順を解説する](https://miyashimo-studio.jp/blog/detail/vscode-install/)
+- vscodeのインストール＋pythonのインストール方法(Windows)  
+    [Visual Studio CodeをインストールしてPython開発環境を準備する(Windows編)](https://sukkiri.jp/technologies/processors/python/visual-studioをインストールしてpython開発環境を準備するwindows編.html)
+- pythonのバージョンについて  
+    pythonはそのバージョンごとにインストールできるライブラリが異なるのでご注意ください．
+    本発表のアプリ開発には, _python 3.12.0_ を使用しています．
+- pipのバージョンについて  
+    pythonのライブラリをインストールする際，パッケージ管理システムpipを利用します．
+    本発表のアプリ開発には, _pip 24.2_ を使用しています．pipの利用については，
+    次のリンク先の『3.3. packageのインストール』をご覧ください．  
+    [Qiita:[python] pip/VSCode開発環境の構築 (windows11)](https://qiita.com/flcn-x/items/ac6e222004a827f582ea)
+- streamlitのバージョンについて  
+    streamlitは現在も活発な開発が行われており，バージョンごとに使えるオプションが異なります．
+    streamlitのAPI referenceで紹介されている機能は，概ね最新版のものです．
+    本発表のアプリ開発には, _streamlit 1.38.0_ を使用しています．
+- streamlitの基本的な使い方について  
+    streamlitのAPI referenceで詳しい説明がなされています．導入については，
+    _Get started_に記載してあります．
+    [streamlit API reference](https://docs.streamlit.io)
+- streamlit Community Cloud を利用した一般公開について  
+    streamlit Community Cloudを利用することで作成したアプリを一般公開することが可能です．
+    そのための手順は次の通りです．
+    1. GitHubのアカウント作成
+    1. GitHubにリポジトリを作成
+    1. GitHubのリポジトリにWebアプリのソースファイル等をアップロード
+    1. streamlit Community Cloudのアカウント作成
+    1. streamlit Community CloudでWebアプリをデプロイ
+"""
