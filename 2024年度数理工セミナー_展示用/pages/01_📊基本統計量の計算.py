85c2bc8bf58af6efd23d450354b01686152567f1
Shuji-Watanabe <shuji-watanabe@neptune.kanazawa-it.ac.jp>
2025-02-20 11:26:56 +0900

    today_ver003

--- 2024年度数理工セミナー_展示用/pages/01_📊基本統計量の計算.py
+++ 2024年度数理工セミナー_展示用/pages/01_📊基本統計量の計算.py
@@ -54,10 +54,11 @@ tmp_sidebar_text = "基本統計量計算のオプション"
 tmp_data_path = "sample_datas/scatter_data01.csv"
 tmp_data_encoding = 'shift_jis'
 
+st.write(tmp_cd_path)
 data_df = Df.data_load_form(sidebar_text=tmp_sidebar_text
-                  , cd_path=tmp_cd_path
-                  , data_path= tmp_data_path
-                  , data_encoding=tmp_data_encoding)
+                            , cd_path=tmp_cd_path
+                            , data_path= tmp_data_path
+                            , data_encoding=tmp_data_encoding)
 st.divider()
 
 
