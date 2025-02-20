45b0dd298a7eb2adf66e5919fee5e6c1bac4fb76
Shuji-Watanabe <shuji-watanabe@neptune.kanazawa-it.ac.jp>
2025-02-20 18:55:19 +0900

    ver045

--- 2024年度数理工セミナー_展示用/00_Data_Analysis_Apps.py
+++ 2024年度数理工セミナー_展示用/00_Data_Analysis_Apps.py
@@ -14,6 +14,15 @@ st.sidebar.markdown(\
 main_title = "データ分析支援アプリ:male-teacher:"
 st.title(f"{main_title}")
 
+st.page_link("00_Data_Analysis_Apps.py", label="Home", icon="🏠")
+st.page_link("pages/01_📊基本統計量の計算.py", label="基本統計量の計算へ", icon="📊")
+st.page_link("pages/02_📊Histgram.py", label="ヒストグラムの作成へ", icon="📊")
+st.page_link("pages/03_📊散布図_散布図行列.py", label="散布図または散布図行列の作成へ", icon="📊")
+st.page_link("pages/04_📊相関係数行列.py", label="相関係数行列の計算へ", icon="📊")
+st.page_link("pages/05_📊偏相関係数行列.py", label="偏相関係数行列の計算へ", icon="📊")
+st.page_link("pages/06_📊線形回帰分析.py", label="線形回帰分析へ", icon="📊")
+
+
 
 ## URLとQRコードの表示
 st.subheader("Network URL 情報", divider="rainbow")
@@ -63,11 +72,4 @@ with right_col:
         st.image(image_path,caption="操作３")
 
 
-st.page_link("00_Data_Analysis_Apps.py", label="Home", icon="🏠")
-st.page_link("pages/01_📊基本統計量の計算.py", label="基本統計量の計算へ", icon="📊")
-st.page_link("pages/02_📊Histgram.py", label="ヒストグラムの作成へ", icon="📊")
-st.page_link("pages/03_📊散布図_散布図行列.py", label="散布図または散布図行列の作成へ", icon="📊")
-st.page_link("pages/04_📊相関係数行列.py", label="相関係数行列の計算へ", icon="📊")
-st.page_link("pages/05_📊偏相関係数行列.py", label="偏相関係数行列の計算へ", icon="📊")
-st.page_link("pages/06_📊線形回帰分析.py", label="線形回帰分析へ", icon="📊")
 
