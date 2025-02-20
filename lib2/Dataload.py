c8701728feaeb5d37f88b56919d2f38dd019c0e6
Shuji-Watanabe <shuji-watanabe@neptune.kanazawa-it.ac.jp>
2025-02-20 16:12:20 +0900

    ver029

---
+++ lib2/Dataload.py
@@ -0,0 +1,62 @@
+def data_load_form(sidebar_text, cd_path, data_path , data_encoding, data_dict):
+    import streamlit as st
+    import pandas as pd
+    import os
+    #=====使用データの設定=====================================================================
+    st.sidebar.subheader(sidebar_text)
+    st.sidebar.markdown(":arrow_forward: 分析に使用するデータの選択")
+    tub_dict = {"分析体験デモデータ":0,"ユーザーデータ":1}
+    selected_cbox = st.sidebar.radio(label="選択", options = tub_dict.keys(),horizontal=True)
+    """ """
+    ###  デモデータによる分析体験
+    tub_counta = 0
+    if tub_dict[selected_cbox] == 0 :
+        tub_title = list(tub_dict.keys())[tub_counta]
+        # with tub_list[0]:
+        st.header(f""":bar_chart: {tub_title }を用いた分析""",divider="rainbow")
+        """   """
+        st.subheader(f"Step１: 分析データの選択", divider="green")
+        select_data_dict = data_dict
+        # 分析データの選択      
+        select_str = st.selectbox("分析するデータファイルを選択してください",select_data_dict.keys(),key="mselect 01")      
+        filedir_path = os.path.join(cd_path,data_path) 
+        csv_path = os.path.join(filedir_path,data_dict[select_str])
+        st.write(csv_path)
+        read_data_df = pd.read_csv( str(csv_path),encoding=data_encoding)
+
+
+    ###  ユーザーデータによる分析体験
+    elif tub_dict[selected_cbox] == 1 :
+        tub_counta += 1
+        tub_title = list(tub_dict.keys())[tub_counta]
+        # with tub_list[1]:
+        st.header(f""":bar_chart: {tub_title }を用いた分析""",divider="rainbow")
+        """   """
+        st.subheader(f"Step１: 分析データのアップロード", divider="green")
+        
+        disp_col0 = st.columns([1,3])
+        with disp_col0[1]:
+            st.warning("データをアップロードする前に，アップロードするデータに個人情報等，取扱に注意しなければならないデータが含まれていないか確認してください．")
+        with disp_col0[0]:
+            tmp_check = st.checkbox("確認しました")
+        st.sidebar.divider()
+
+        if tmp_check:
+            st.divider()
+            uploaded_files = st.sidebar.file_uploader(":arrow_forward: CSVファイルのアップロード")    
+            if not uploaded_files:
+                st.sidebar.error('データがアップロードされていません', icon="⚠️")
+                st.stop()
+        else :
+            st.sidebar.write("停止中")
+            st.stop()
+        
+        st.sidebar.divider()
+        set_encode_list = ["自動","選択","入力"]
+        selected_way = st.sidebar.radio(":arrow_forward: エンコードの指定方法",options=set_encode_list,horizontal=True)
+        import lib.FileProcessing as FileProcessing 
+        read_data_df= FileProcessing.streamlit_uploaded_csv(st_uploaded_files=uploaded_files
+                                                            ,selected_type=selected_way
+                                                            ,DisplayLocation="sidebar")
+
+    return read_data_df
---
+++ lib2/display.py
@@ -0,0 +1,76 @@
+def display_URL_QRCode(location_str):
+    import os
+    import socket
+
+    if location_str == "streamlit_Community_Cloud": 
+        network_url = "https://kit-msec-watanabe-data-analysis-apps.streamlit.app"
+    else :
+        path = os.getcwd()
+        data_path = path+"/基本データ"
+
+        
+        # ホストIPアドレスを取得
+        def get_host_ip():
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            try:
+                # GoogleのDNSサーバーに接続（IPアドレスは実際には送信されない）
+                s.connect(("8.8.8.8", 80))
+                ip = s.getsockname()[0]
+            except Exception:
+                ip = "127.0.0.1"  # デフォルト（ローカル）
+            finally:
+                s.close()
+            return ip
+
+        # ポート番号を取得
+        port = os.environ.get("STREAMLIT_SERVER_PORT", "8501")
+        host_ip = get_host_ip()
+        network_url = f"http://{host_ip}:{port}"
+        
+    # Network URL を表示
+    from qrcode import QRCode
+    qr = QRCode()
+    qr.add_data(network_url)
+    qr.make()  # QRコードを生成
+    qr_image = qr.make_image()  # QRコードの画像を生成
+    return network_url, qr_image
+
+
+def explanation_corr(val):
+    if  0 <= val :
+        tmp_str = "正"
+    else :
+        tmp_str = "負"
+        
+    if 0.7 <= abs(val) <= 1.0:
+        exp_str = f"強い{tmp_str}の相関がある"
+    elif 0.4 <= abs(val) < 0.7:
+        exp_str = f"中程度の{tmp_str}の相関がある"
+    elif 0.2 <= abs(val) < 0.4:
+        exp_str = f"弱いの{tmp_str}の相関がある"
+    elif 0.0 <= abs(val) < 0.2:
+        exp_str = f"ほぼ相関はない"
+    return exp_str
+
+def make_lr_expr(names_list,response_value,coef_list):
+    ## １．数式の生成
+    # LaTeX文字列を作成する
+    terms = []
+    for i, item in enumerate(names_list):
+        # 小数第3位までの絶対値
+        formatted_coeff = f"{abs(coef_list[i]):.3f}"
+        # 1つ目の項目は符号なし（負の場合は先頭に '-' を付ける）
+        if i == 0:
+            sign = "-" if coef_list[i] < 0 else ""
+        else:
+            # 2つ目以降は正の場合は " + ", 負の場合は " - "
+            sign = " - " if coef_list[i]  < 0 else " + "
+        # 各項目の項を生成 (例: "1.23\,(item1)")
+        if i == 0:
+            term = f"{sign}{formatted_coeff}"
+        else:
+            term = f"{sign}{formatted_coeff}\\big(\\text{{{str(item)}の値}}\\big)"
+        terms.append(term)
+    # 連結して最終的な文字列を作成
+    exp_latex = f"\\big(\\text{{{response_value}の値}}\\big)"" = " + "".join(terms)
+    return exp_latex
